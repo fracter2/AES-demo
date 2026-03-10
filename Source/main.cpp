6d746032ef03a96f4770932088f5df92b357cce9
Theodor <fracter.giftogod@gmail.com>
2026-03-10 23:12:17 +0100

    Update project description in main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -21,11 +21,8 @@
 *
 ********************************************************************************************/
 
-// This project is a "best practice" refactor of Liv and Eric's Space Invaders remake, which
-// itself is based on the source code of "raylib [core] example - Basic window", as seen 
-// above.
-//
-// Refactor by Theodor Rydberg, for the final asignment of the "Modern c++" course 2025-2026
+// This project is an implementation of AES by Theodor Rydberg (github.com/fracter2).
+// It's my final assignment hand-in of the computer security course I am taking.
 
 #include <iostream>
 #include "raylib.h"
@@ -53,14 +50,3 @@ int main(void)
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
