844911bd1e9e7d03e8ffd1caf77876559c8e4bac
Theodor <fracter.giftogod@gmail.com>
2026-03-19 01:51:02 +0100

    Implement shift rows step + tests to aes.cpp

--- Source/aes.cpp
+++ Source/aes.cpp
@@ -7,7 +7,10 @@ namespace {
 
 	void ShiftRows(Block& block) 
 	{ 
-		// TODO Shift each row LEFT by it's row index... so row0 zero times, row 1 once...
+		// NOTE First row (0 to 4) is skipped
+		std::rotate(block.begin() + 4, block.begin() + 4 + 1, block.begin() + 8);
+		std::rotate(block.begin() + 8, block.begin() + 8 + 2, block.begin() + 12);
+		std::rotate(block.begin() + 12, block.begin() + 12 + 3, block.begin() + 16);
 	}
 
 	void MixColumns(Block& block)
@@ -34,6 +37,24 @@ namespace {
 // Final-round - Skip step 3
 
 // Each round has these 4 steps
+TEST_CASE("aes-ShiftRows") {
+	const Block rising = { 
+		0_b, 1_b, 2_b, 3_b, 
+		4_b, 5_b, 6_b, 7_b,
+		8_b, 9_b, 10_b, 11_b,
+		12_b, 13_b, 14_b, 15_b
+	};
+	const Block risingShifted = {
+		0_b, 1_b, 2_b, 3_b,
+		5_b, 6_b, 7_b, 4_b,
+		10_b, 11_b, 8_b, 9_b,
+		15_b, 12_b, 13_b, 14_b
+	};
+
+	Block temp = rising;
+	ShiftRows(temp);
+	CHECK(temp == risingShifted);
+}
 
 // Step 1 - Sub Bytes
 // Using an S-box lookup table
